487c9415e167d4a8f0127674caa0022d4659b5b5
GwenDiky <sefora.abigail18@gmail.com>
2023-08-10 17:20:34 +0300

    Initial commit

--- manager/manager/urls.py
+++ manager/manager/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from main.views import index
 from django.contrib.sitemaps.views import sitemap
 from main.sitemaps import TaskSitemap
+from django.conf import settings
+from django.conf.urls.static import static
 
 sitemaps = {
     'tasks': TaskSitemap,
@@ -17,3 +19,7 @@ urlpatterns = [
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, 
+                        document_root = settings.MEDIA_ROOT)
